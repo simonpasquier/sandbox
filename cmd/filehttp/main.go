c195b74321b6e352008920e35356d46ea9fa57b5
Simon Pasquier <pasquier.simon@gmail.com>
2018-02-20 15:13:29 +0100

    Add filehttp program to serve a static file over HTTP

---
+++ cmd/filehttp/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
+
+var (
+	help         bool
+	listen, file string
+)
+
+func init() {
+	flag.BoolVar(&help, "help", false, "Help message")
+	flag.StringVar(&listen, "listen-address", ":8080", "Listen address")
+	flag.StringVar(&file, "file", "", "File to serve")
+}
+
+func main() {
+	flag.Parse()
+	if help {
+		fmt.Fprintln(os.Stderr, "Simple HTTP server displaying the incoming request")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
+
+	if file == "" {
+		fmt.Fprintln(os.Stderr, "Missing --file parameter.")
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+
+	b, err := ioutil.ReadFile(file)
+	if err != nil {
+		log.Fatal("Error reading", file, ":", err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		_, err := w.Write(b)
+
+		if err != nil {
+			log.Println("Failed to write response:", err)
+		}
+	})
+
+	log.Println("Listening on", listen)
+	log.Fatal(http.ListenAndServe(listen, nil))
+}
