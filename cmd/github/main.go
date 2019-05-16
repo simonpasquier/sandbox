ab79831f1dc15241a5ecaa653b2408584edadf70
Simon Pasquier <spasquie@redhat.com>
2019-05-16 15:49:11 +0200

    cmd/github: make it more generic

--- cmd/github/main.go
+++ cmd/github/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"os"
+	"regexp"
+	"strconv"
 	"strings"
+	"text/template"
+	"time"
 
-	"github.com/google/go-github/github"
+	"github.com/google/go-github/v25/github"
 	"github.com/jszwedko/go-circleci"
 	"github.com/pkg/errors"
 	"golang.org/x/oauth2"
@@ -19,6 +24,7 @@ var (
 	help             bool
 	ghtPath, cctPath string
 	org              string
+	repoNames        string
 )
 
 func init() {
@@ -26,6 +32,79 @@ func init() {
 	flag.StringVar(&cctPath, "circleci.token", "", "Path to your Circle CI token")
 	flag.StringVar(&ghtPath, "github.token", "", "Path to your GitHub token")
 	flag.StringVar(&org, "github.organization", "prometheus", "Name of the GitHub organization")
+	flag.StringVar(&repoNames, "github.repositories", "", "Comma-separated list of the repositories to filter on (default: all)")
+}
+
+type travisSettings struct {
+	Enabled bool
+}
+
+type circleSettings struct {
+	Enabled bool
+	Flags   circleci.FeatureFlags
+}
+
+type integration struct {
+	Active    bool
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	Events    []string
+	ID        string
+	URL       string
+}
+
+//TODO: support additional fields once supported by the GitHub API client.
+type key struct {
+	//CreatedAt time.Time
+	Name string
+	//Verified bool
+	ReadOnly bool
+}
+
+type repository struct {
+	Name         string
+	Integrations []integration
+	Keys         []key
+	Circle       circleSettings
+	Travis       travisSettings
+}
+
+const repoTmpl = `Repository: {{ .Name }}
+
+Integrations:{{ range .Integrations }}
+- ID: {{ .ID }}
+  Active: {{ if .Active }}yes{{ else }}no{{ end }}
+  URL: {{ .URL }}
+  Events: {{ join .Events "," }}
+  Created at: {{ .CreatedAt }}
+  Updated at: {{ .UpdatedAt }}
+{{- end }}
+
+Keys:{{ range .Keys }}
+- {{ .Name }} ({{ if .ReadOnly }}read-only{{ else }}read-write{{ end }})
+{{- else }} none{{- end }}
+
+CircleCI:{{ if .Circle.Enabled }}
+- OSS: {{ .Circle.Flags.OSS }}
+  Build fork PRs: {{ .Circle.Flags.BuildForkPRs }}
+{{ else }} not enabled{{ end }}
+
+TravisCI:{{ if .Travis.Enabled }}
+-
+{{ else }} not enabled{{ end }}`
+
+func (r *repository) String() string {
+	t := template.New("repository").Funcs(template.FuncMap{"join": strings.Join})
+	t, err := t.Parse(repoTmpl)
+	if err != nil {
+		return err.Error()
+	}
+	b := bytes.Buffer{}
+	err = t.Execute(&b, r)
+	if err != nil {
+		return err.Error()
+	}
+	return b.String()
 }
 
 func checkMark(s string, ok bool) {
@@ -63,15 +142,19 @@ func main() {
 	if cctPath == "" {
 		log.Println("WARN: circleci.token parameter is missing")
 	}
-	if err := run(org, ghtPath, cctPath); err != nil {
+	repos, err := run(org, ghtPath, cctPath)
+	if err != nil {
 		log.Fatal(err)
+	}
+	for _, repo := range repos {
+		fmt.Println(repo.String())
 	}
 }
 
-func run(org string, ghToken string, ccToken string) error {
+func run(org string, ghToken string, ccToken string) ([]repository, error) {
 	ght, err := readTokenFile(ghToken)
 	if err != nil {
-		return err
+		return nil, err
 	}
 	ctx := context.Background()
 	ghc := github.NewClient(
@@ -87,97 +170,98 @@ func run(org string, ghToken string, ccToken string) error {
 	if ccToken != "" {
 		cct, err := readTokenFile(ccToken)
 		if err != nil {
-			return err
+			return nil, err
 		}
 		ccc = &circleci.Client{Token: cct}
 	}
 
-	var repos = make([]*github.Repository, 0)
+	var ghRepos = make([]*github.Repository, 0)
 	opt := &github.RepositoryListByOrgOptions{
 		ListOptions: github.ListOptions{PerPage: 10},
 	}
 	for {
 		r, resp, err := ghc.Repositories.ListByOrg(ctx, org, opt)
 		if err != nil {
-			return errors.Wrap(err, "Fail to list GitHub repositories")
+			return nil, errors.Wrap(err, "Fail to list GitHub repositories")
 		}
-		repos = append(repos, r...)
+		if repoNames != "" {
+			for _, r := range r {
+				re := regexp.MustCompile("(?:^|,)" + r.GetName() + "(,|$)")
+				if re.MatchString(repoNames) {
+					ghRepos = append(ghRepos, r)
+				}
+			}
+		} else {
+			ghRepos = append(ghRepos, r...)
+		}
 		if resp.NextPage == 0 {
 			break
 		}
 		opt.Page = resp.NextPage
 	}
-	fmt.Printf("✔ Found %d repositories in GitHub\n\n", len(repos))
 
 	cc := make(map[string]circleci.FeatureFlags)
 	if ccc != nil {
 		ccp, err := ccc.ListProjects()
 		if err != nil {
-			return errors.Wrap(err, "fail to list CircleCI projects")
+			return nil, errors.Wrap(err, "fail to list CircleCI projects")
 		}
 		for _, p := range ccp {
-			if "prometheus" == p.Username {
+			if org == p.Username {
 				cc[p.Reponame] = p.FeatureFlags
 			}
 		}
 	}
 
-	for _, r := range repos {
-		fmt.Println("Repository:", r.GetName())
+	repos := make([]repository, 0, len(ghRepos))
+	for _, r := range ghRepos {
+		repo := repository{Name: r.GetName()}
 
-		fmt.Println()
-		hooks, _, err := ghc.Repositories.ListHooks(ctx, "prometheus", r.GetName(), nil)
+		hooks, _, err := ghc.Repositories.ListHooks(ctx, org, r.GetName(), nil)
 		if err != nil {
-			return errors.Wrap(err, "fail to list hooks")
+			return nil, errors.Wrap(err, fmt.Sprintf("%s: fail to list hooks", r.GetName()))
+		}
+		repo.Integrations = make([]integration, 0, len(hooks))
+		for _, h := range hooks {
+			var u string
+			if _, ok := h.Config["url"]; ok {
+				u, _ = h.Config["url"].(string)
+			}
+			repo.Integrations = append(repo.Integrations, integration{
+				ID:        strconv.FormatInt(h.GetID(), 10),
+				Active:    h.GetActive(),
+				Events:    h.Events,
+				CreatedAt: h.GetCreatedAt(),
+				UpdatedAt: h.GetUpdatedAt(),
+				URL:       u,
+			})
 		}
 
 		//TODO: list branch settings.
-		//TODO: list deploy keys.
+		branches, _, err := ghc.Repositories.ListBranches(ctx, org, r.GetName(), nil)
+
 		//TODO: list security alerts.
-
-		fmt.Println("External integrations:")
-		for _, h := range hooks {
-			fmt.Println("• ID:", h.GetID())
-			var (
-				name    string
-				isValid bool
-			)
-			if h.GetName() == "web" {
-				if u, ok := h.Config["url"]; ok {
-					s, ok := u.(string)
-					if ok {
-						name = s + " (generic webhook)"
-						isValid = true
-
-					} else {
-						name = "/!\\ URL field isn't a string"
-					}
-				} else {
-					name = "/!\\ No URL in configuration"
-				}
-			} else {
-				name = h.GetName() + " (GitHub service)"
-				isValid = true
-			}
-			checkMark(name, isValid)
-			checkMark("active", h.GetActive())
-			checkMark(fmt.Sprintf("events: %s", strings.Join(h.Events, ",")), true)
-		}
 		//TODO: list installed apps.
+		keys, _, err := ghc.Repositories.ListKeys(ctx, org, r.GetName(), nil)
+		repo.Keys = make([]key, 0, len(keys))
+		for _, k := range keys {
+			repo.Keys = append(repo.Keys, key{
+				Name:     k.GetTitle(),
+				ReadOnly: k.GetReadOnly(),
+			})
+		}
 
 		if len(cc) > 0 {
-			fmt.Println()
-			fmt.Println("CircleCI")
 			ccFlags, ok := cc[r.GetName()]
-			if !ok {
-				checkMark("Project not found in CircleCI", false)
-			} else {
-				checkMark("Build fork PRs", ccFlags.BuildForkPRs)
-				checkMark("OSS", ccFlags.OSS)
+			if ok {
+				repo.Circle = circleSettings{
+					Enabled: true,
+					Flags:   ccFlags,
+				}
 			}
 		}
 
-		fmt.Println()
+		repos = append(repos, repo)
 	}
-	return nil
+	return repos, nil
 }
